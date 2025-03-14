67ce6a1f35ee48a9ff7e11164becdf2eac539be3
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-03-14 23:56:38 +0100

    Code: ROOT_URLCONF and TEMPLATES in settings.py

--- serious_talk/settings.py
+++ serious_talk/settings.py
@@ -34,3 +34,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+ROOT_URLCONF = 'project_4.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
