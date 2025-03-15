8a65f79f8ad79d4501592738a19424bcbf05fb91
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-03-15 15:00:06 +0100

    Code: Add Category model for organizing posts.

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Category nodel for organizing posts by topic.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
