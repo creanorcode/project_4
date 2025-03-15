9942b5daf9fd0763320d1656660610af47ed48bd
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-03-15 15:52:42 +0100

    Code inprovements according to Flake8 in models.py

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Category nodel for organizing posts by topic.
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -8,19 +9,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-# Post model representing individual posts with title, content, and vote tracking.
+
+# Post model representing individual
+# posts with title, content, and vote tracking.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True, blank=True)
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
-    
+
     def vote_score(self):
         return self.upvotes - self.downvotes
-
