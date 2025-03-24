10c8c24121cb3487b9fece0da6486d7c74820d0e
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-03-24 22:51:50 +0100

    Code improvement according to Flake8 in manage.py.

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import os   # Import operating system utilities.
 import sys  # Import system-specific parameters and functions.
 
+
 def main():
     # Set the default Django settings module for the 'manage.py' script.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_4.settings')
@@ -12,11 +13,13 @@ def main():
     except ImportError as exc:
         # If Django is not installed or available, raise a clear error.
         raise ImportError(
-            "Couldn´t import Django. Are you sure it´s installed and available on your PYTHONPATH"
+            "Couldn´t import Django. Are you sure it´s \
+            installed and available on your PYTHONPATH"
             "Did you forget to activate a virtual environment?"
             ) from exc
     # Execute the command-line utility with the arguments provided.
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
